e84acfe448750d9ac4eeb6563bd6aa085418d080
William8Bit <dev.william.amaral@gmail.com>
2023-02-20 13:51:26 -0300

    Segunda versão do codigo

--- Usuarios/usuarios.sh
+++ Usuarios/usuarios.sh
@@ -4,7 +4,23 @@
 # Mostra os logins e nomes dos usuários do sistema
 # Obs.: Lê dados do arquivo /etc/passwd
 #
-# Skulkills, Fevereiro de 2007
+# Versão 1: Mostra o usuários e nomes separados por TAB
+# Versão 2: Adiciona suporte à opção -h
 #
+# Skulkills, Fevereiro de 2023
+
+MENSAGEM_USO="
+Uso: $0 [-h]
+        -h      Mostra esta tela de ajuda e sai
+"
+
+# Tratamento das opções de linha de comando
+if test "$1" = "-h"
+then
+    echo "$MENSAGEM_USO"
+    exit 0
+fi
+
+# Processamento
 cut -d : -f 1,5 /etc/passwd | tr : \\t
 
