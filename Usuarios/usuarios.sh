5ff778b7e5547a9d75a5df963fe3938cadada8b7
William8Bit <dev.william.amaral@gmail.com>
2023-02-20 13:53:49 -0300

    Terceira versão do codigo

--- Usuarios/usuarios.sh
+++ Usuarios/usuarios.sh
@@ -6,20 +6,33 @@
 #
 # Versão 1: Mostra o usuários e nomes separados por TAB
 # Versão 2: Adiciona suporte à opção -h
+# Versão 3: Adiciona suporte à opção -V e opções inválidas
 #
 # Skulkills, Fevereiro de 2023
 
 MENSAGEM_USO="
-Uso: $0 [-h]
+Uso: $0 [-h | -V]
         -h      Mostra esta tela de ajuda e sai
+        -V      Mostra a versão do programa e sai
 "
 
 # Tratamento das opções de linha de comando
-if test "$1" = "-h"
-then
-    echo "$MENSAGEM_USO"
-    exit 0
-fi
+case "$1" in
+    -h)
+        echo "$MENSAGEM_USO"
+        exit 0
+    ;;
+
+    -V)
+        echo $0 Versão 3
+        exit 0
+    ;;
+
+    *)
+        echo Opção inválida: $1
+        exit 0
+    ;;
+esac
 
 # Processamento
 cut -d : -f 1,5 /etc/passwd | tr : \\t
